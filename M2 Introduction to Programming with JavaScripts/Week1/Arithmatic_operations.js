35d78b1e81c1959df5e0446d35b8aa0b1679f0d7
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-18 21:59:33 +0530

    Create Arithmatic_operations.js

---
+++ M2 Introduction to Programming with JavaScripts/Week1/Arithmatic_operations.js
@@ -0,0 +1,7 @@
+console.log(2+2); //addition
+console.log(10-2); //subtraction
+console.log(3*2); //multiplication
+console.log(10/2); //division
+console.log(2 > 3); //comparing - return false since 2 is not greater than 3
+console.log(3 > 2); //comparing - return true since 3 is greater than 2
+console.log(10 == 10); //comparing - return true since 10 is equal to 10
