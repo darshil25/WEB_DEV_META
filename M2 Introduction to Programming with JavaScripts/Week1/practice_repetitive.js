9ab79b6200ac5aac3e1cfa498b74a74688253dd8
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-19 22:45:01 +0530

    Create practice_repetitive.js

---
+++ M2 Introduction to Programming with JavaScripts/Week1/practice_repetitive.js
@@ -0,0 +1,29 @@
+for(var i=1; i<6; i++){
+    console.log(i);
+}
+console.log('Counting completed');
+
+for(var i=5; i>0; i--){
+    console.log(i);
+}
+console.log('Counting completed');
+
+var j = 1;
+while(j < 6){
+    console.log(j);
+    j++;
+}
+console.log('Counting completed');
+
+var a = 5;
+while(a > 0){
+    console.log(a);
+    a--;
+}
+console.log('Counting completed');
+
+var b = 2018;
+while(b < 2023){
+    console.log(b);
+    b++;
+}
