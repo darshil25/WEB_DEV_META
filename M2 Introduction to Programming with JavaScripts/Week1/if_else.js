e904326b4b800caa3ae4c67b1321cbbdafa0742f
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-19 22:16:32 +0530

    added files

---
+++ M2 Introduction to Programming with JavaScripts/Week1/if_else.js
@@ -0,0 +1,14 @@
+var result = 50;
+result = 25;
+if(result > 40){
+    console.log("You have passed the exam");
+}
+else if(result >30 && result < 40){
+    console.log("You will get 2 chances");
+}
+else if(result >20 && result < 30){
+    console.log("You will get 1 chance");
+}
+else{
+    console.log("You have failed the exam");
+}
---
+++ M2 Introduction to Programming with JavaScripts/Week1/switch.js
@@ -0,0 +1,18 @@
+var place = "second";
+
+switch(place){
+    case 'first':
+        console.log("Won Gold");
+        break;
+    
+    case 'second':
+        console.log("Won Silver");
+        break;    
+
+    case 'third':
+        console.log("Won Bronze");
+        break;
+
+    default:
+        console.log("Lost");
+}
