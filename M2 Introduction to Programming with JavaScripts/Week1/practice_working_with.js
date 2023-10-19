42f07992e22a51ce97f62a809b2a886915a54b31
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-19 22:58:20 +0530

    Create practice_working_with.js

---
+++ M2 Introduction to Programming with JavaScripts/Week1/practice_working_with.js
@@ -0,0 +1,31 @@
+for(var i = 1; i<11; i++){
+
+    if(i == 1){
+        console.log("Gold medal");
+    }
+    else if(i == 2){
+        console.log("Silver medal");
+    }
+    else if(i == 3){
+        console.log("Bronze medal");
+    }
+    else{
+        console.log(i);
+    }
+}
+
+for(var i = 1; i<11; i++){
+    switch(i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i);
+    }
+}
