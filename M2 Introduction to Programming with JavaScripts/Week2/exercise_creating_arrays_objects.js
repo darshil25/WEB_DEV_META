23f950d829575a5f405723256d38fcdda6922b02
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-22 23:47:29 +0530

    files for module 2

---
+++ M2 Introduction to Programming with JavaScripts/Week2/exercise_creating_arrays_objects.js
@@ -0,0 +1,14 @@
+var clothes = [];
+clothes.push("pant");
+clothes.push("chaddo");
+clothes.push("tshirt");
+clothes.push("ganji");
+clothes.push("lengho");
+clothes.pop();
+clothes.push("oversized tshirt");
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "brown";
+favCar.covertible = false;
+console.log(favCar);
---
+++ M2 Introduction to Programming with JavaScripts/Week2/typeOf.js
@@ -0,0 +1,23 @@
+var test = typeof('maru nam');
+console.log(test);
+
+var test = typeof(10);
+console.log(test);
+
+var test = typeof(0.4);
+console.log(test);
+
+var test = typeof(true);
+console.log(test);
+
+var test = typeof(1 < 2);
+console.log(test);
+
+var test = typeof([7, 8, 9]);
+console.log(test);
+
+var test = typeof({firstproperty : 1});
+console.log(test);
+
+var test = typeof(function a(){console.log('kem 6o');});
+console.log(test);
