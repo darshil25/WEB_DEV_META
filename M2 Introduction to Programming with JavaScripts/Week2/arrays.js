f87ee48f25d29c00e6fdac8cb850a9469969c6a9
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-22 23:09:50 +0530

    Create arrays.js

---
+++ M2 Introduction to Programming with JavaScripts/Week2/arrays.js
@@ -0,0 +1,11 @@
+var arr = [];
+// we can use push for input and pop for remove element from array
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+console.log(arr);
+
+arr.pop(3);
+console.log(arr);
