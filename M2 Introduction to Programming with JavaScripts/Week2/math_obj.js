2c13a30122f6bf0b2023b8f2f1bed79d245a1045
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-22 23:15:15 +0530

    Create math_obj.js

---
+++ M2 Introduction to Programming with JavaScripts/Week2/math_obj.js
@@ -0,0 +1,8 @@
+var num = Math.random(); //math.random gives random number between 0 to 0.99
+
+console.log(num);
+console.log(num*100);
+
+num = Math.ceil(6.8); //round up to nearest integer
+console.log(num);
+
