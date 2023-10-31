30a659b0dfd2fee46b3d440a41c7923477dac561
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-31 22:08:03 +0530

    added JS in browser

---
+++ M2 Introduction to Programming with JavaScripts/Week3/JavaScript in Browser/greetings.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hello, ${peopl}`);
+}
+
+export const formalGreeting = (person) => {
+    console.log(`Hello Sir/ madam, ${people}`);
+}
+
+const greeting = () => {
+    console.log(`kem 6o`);
+}
+
+export default greeting;
