bb938b46c5ef4800551bd47f6ff5cdf9b7758821
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2024-01-31 22:50:23 +0530

    added lab 1

---
+++ M5 React Basics/week1/components/compo_test_file.js
@@ -0,0 +1,6 @@
+function Heading(){
+    let title = "This is title";
+    return (<h1>{title}</h1>); // JSX -> use {} to print title variable instead of "title" 
+};
+
+export default Heading; // export Heading function to use in other files
